8de6c07fd6c427ffff66075ba480b8234556a860
walpakhart <nswalpakhart@gmail.com>
2024-06-26 22:39:16 +0300

    fix files rewrites

--- task6/cpu_load.sh
+++ task6/cpu_load.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-LOG_FILE_1="/etc/nginx/logs/file1.log"
-LOG_FILE_2="/etc/nginx/logs/file2.log"
-LOG_FILE_3="/etc/nginx/logs/file3.log"
-LOG_FILE_4="/etc/nginx/logs/file4.log"
+while :
+do
+  # Получаем текущую загрузку CPU
+  cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
 
-while true; do
-    if [ $(stat -c%s "$LOG_FILE_1") -gt 10240 ]; then
-        LINE_COUNT=$(wc -l < "$LOG_FILE_1")
-        echo "$(date): Cleared $LOG_FILE_1 with $LINE_COUNT lines" >> $LOG_FILE_2
-        > $LOG_FILE_1
-    fi
+  # Записываем данные в JSON файл
+  echo "{ \"cpuUsage\": \"$cpuUsage\" }" > /var/www/html/cpu/stats.json
 
-    input_file="$LOG_FILE_1"
-    output_file_4xx="$LOG_FILE_4"
-    output_file_5xx="$LOG_FILE_3"
-    while read -r line; do
-        http_code=$(echo "$line" | awk '{print $9}')
-        case "$http_code" in
-            [4]*)
-                echo "$line" >> "$output_file_4xx"
-                ;;
-            [5]*)
-                echo "$line" >> "$output_file_5xx"
-                ;;
-        esac
-    done < "$input_file"
-
-    sleep 5
+  # Ждем 1 секунду перед следующим обновлением
+  sleep 1
 done
+
 
